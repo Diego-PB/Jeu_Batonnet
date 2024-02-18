ebe96958d23081d1b10e51a1e39fea8ef16d8d8c
Diego PB <150288516+Diego-PB@users.noreply.github.com>
2024-02-18 17:53:07 +0100

    setup

---
+++ Batonnet_cmd/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Programmer;
+
+internal class Jeu
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        AffichageMenu();
+        
+        // Saisie clavier
+    }
+
+    public static void AffichageMenu()
+    {
+        Console.WriteLine(@"
+  ____        _                         _   
+ | __ )  __ _| |_ ___  _ __  _ __   ___| |_ 
+ |  _ \ / _` | __/ _ \| '_ \| '_ \ / _ \ __|
+ | |_) | (_| | || (_) | | | | | | |  __/ |_ 
+ |____/ \__,_|\__\___/|_| |_|_| |_|\___|\__|
+                                            
+-------------------------------------------------
+|                   MENU                        |
+-------------------------------------------------
+| 1. Jouer                                     |
+| 2. Options                                   |
+| 3. Scores                                    |
+| 4. Quitter                                   |
+-------------------------------------------------
+");
+    }
+}
