831976335b4ef7ff207bb122679289996f7352f2
Diego PB <150288516+Diego-PB@users.noreply.github.com>
2024-02-18 18:21:47 +0100

    save

--- Batonnet_cmd/Program.cs
+++ Batonnet_cmd/Program.cs
@@ -8,6 +8,28 @@ internal class Jeu
         AffichageMenu();
         
         // Saisie clavier
+		Console.Write("Votre choix : ");
+        string choix = Console.ReadLine();
+        
+        // Traitement du choix
+        switch (choix)
+        {
+            case "1":
+                Console.WriteLine("Jouer");
+                break;
+            case "2":
+                Console.WriteLine("Options");
+                break;
+            case "3":
+                Console.WriteLine("Scores");
+                break;
+            case "4":
+                Console.WriteLine("Quitter");
+                break;
+            default:
+                Console.WriteLine("Choix invalide");
+                break;
+        }
     }
 
     public static void AffichageMenu()
